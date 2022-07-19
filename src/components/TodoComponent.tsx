58d16946f0b5b3f36dc3952c12629521bd1aab92
teo-goulois <teogoulois@gmail.com>
2022-07-19 17:16:43 +0200

    fix color checkbox

--- src/components/TodoComponent.tsx
+++ src/components/TodoComponent.tsx
@@ -7,7 +7,7 @@ import { TrashIcon } from "../utilities/Icons";
 type Props = {
   todo: Todo;
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-  deleteOption?: boolean
+  deleteOption?: boolean;
 };
 const TodoComponent = ({ todo, setTodos, deleteOption }: Props) => {
   const handleChange = () => {
@@ -53,7 +53,7 @@ const TodoComponent = ({ todo, setTodos, deleteOption }: Props) => {
           checked={todo.compleded}
           onChange={handleChange}
           type="checkbox"
-          className="checked:bg-blue-400 "
+          className="appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue  outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer ring-0 focus:ring-offset-0 focus:ring-0"
         />
         <label
           className={`font-medium text-lg ml-1 ${
@@ -64,9 +64,11 @@ const TodoComponent = ({ todo, setTodos, deleteOption }: Props) => {
           {todo.text}
         </label>
       </div>
-      { deleteOption && <button onClick={() => deleteTodo()}>
-        <TrashIcon fill="#BDBDBD" />
-      </button>}
+      {deleteOption && (
+        <button onClick={() => deleteTodo()}>
+          <TrashIcon fill="#BDBDBD" />
+        </button>
+      )}
     </div>
   );
 };
